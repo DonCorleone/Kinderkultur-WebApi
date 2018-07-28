0c7b02db0a4fb9804b30cbd03cde6d2f0742f7b3
DonCorleone <vitocorleone77@gmail.com>
2018-07-28 23:14:19 +0200

    Start with Server & Client debugging :)

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -2,18 +2,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace KinderKulturServer.Extensions
 {
-   public static class ServiceExtensions
-   {
-      public static void ConfigureCors(this IServiceCollection services)
-      {
-         services.AddCors(options =>
-         {
-            options.AddPolicy("CorsPolicy",
-             builder => builder.AllowAnyOrigin()
-             .AllowAnyMethod()
-             .AllowAnyHeader()
-             .AllowCredentials());
-         });
-      }
-   }
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+    }
 }
